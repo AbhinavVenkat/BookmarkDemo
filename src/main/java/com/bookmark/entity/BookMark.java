8483dc6ca0a5fa7379619b32313172248f010da3
AbhinavVenkat <venkatabhinavlv@gmail.com>
2024-12-12 12:10:27 +0530

    Making the necessary changes in Repo file post the change in entity package name.

---
+++ src/main/java/com/bookmark/entity/BookMark.java
@@ -0,0 +1,28 @@
+package com.bookmark.entity;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@Table(name = "bookmark")
+public class BookMark {
+   @Id
+   @GeneratedValue(strategy = GenerationType.AUTO)
+    Integer id;
+    String title;
+    String url;
+    String description;
+    String creationDate;
+}
--- src/main/java/com/bookmark/repo/BookMarkRepository.java
+++ src/main/java/com/bookmark/repo/BookMarkRepository.java
@@ -2,7 +2,7 @@ package com.bookmark.repo;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import com.bookmark.model.BookMark;
+import com.bookmark.entity.BookMark;
 
 @Repository
 public interface BookMarkRepository extends JpaRepository<BookMark, Integer> {
