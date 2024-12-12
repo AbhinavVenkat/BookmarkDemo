bb32c53ee87973041b26e7263c05e2894f76e493
AbhinavVenkat <venkatabhinavlv@gmail.com>
2024-12-12 12:07:22 +0530

    Formatting build.gradle file and renaming the entity package

--- src/main/java/com/bookmark/controllers/BookMarkController.java
+++ src/main/java/com/bookmark/controllers/BookMarkController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.PutMapping;
 import io.swagger.v3.oas.annotations.parameters.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import com.bookmark.model.BookMark;
+import com.bookmark.entity.BookMark;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
